ae8953c6a667d2447540132d6a5d48def70a7898
Ravi Kumar <ravib@alohatechnologydev.com>
2017-10-31 18:02:08 +0530

    initial employee form design

--- app/assets/javascripts/employee.js
+++ app/assets/javascripts/employee.js
@@ -6,4 +6,15 @@
       autoclose: true,
     };
     date_input.datepicker(options);
+
+
+	  $.uploadPreview({
+	    input_field: "#image-upload",
+	    preview_box: "#image-preview",
+	    label_field: "#image-label"
+	  });
+
+	  $('#employee_table').DataTable();
+
   });
+
