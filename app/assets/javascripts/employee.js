2988cf275a816b02675ec393e7e98baba73a361e
Ravi Kumar <ravib@alohatechnologydev.com>
2017-10-31 21:22:15 +0530

    Added new Change Lead pop-up & etc

--- app/assets/javascripts/employee.js
+++ app/assets/javascripts/employee.js
@@ -1,20 +1,32 @@
+ jQuery(function() {
+
+  $('#employee_table').DataTable();
+  $('#projects_history_table').dataTable();
+  $('#team_members_table').dataTable();
+
+ });
+
  $(document).ready(function(){
-    var date_input=$('.datePicker');
-    var options={
-      format: 'dd-mm-yyyy',
-      todayHighlight: true,
-      autoclose: true,
-    };
-    date_input.datepicker(options);
 
+  var date_input=$('.datePicker');
+  var options={
+    format: 'dd-mm-yyyy',
+    todayHighlight: true,
+    autoclose: true,
+  };
+  date_input.datepicker(options);
 
-	  $.uploadPreview({
-	    input_field: "#image-upload",
-	    preview_box: "#image-preview",
-	    label_field: "#image-label"
-	  });
-
-	  $('#employee_table').DataTable();
-
+  $.uploadPreview({
+    input_field: "#image-upload",
+    preview_box: "#image-preview",
+    label_field: "#image-label"
   });
 
+  $("#image-upload").on("click", function() {
+    var backgroundImage = $('#image-preview').css('background-image');
+    var imageName = backgroundImage.replace('url(','').replace(')','').replace(/\"/gi, "").split('/').pop();
+    $('#imagepreview').attr('src', '/'+imageName);
+    $('#editProfilePopUp').modal('show');
+  });
+
+});
